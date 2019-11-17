c2d97f601d23c8392bb031f9c83f14b7135dd414
Holben888 <holben888@gmail.com>
2019-11-17 04:28:37 -0500

    get user store working

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Account from './accountSetup'
 import AddRoom from './addRoom'
 import Genre from './genre'
 import Room from './room'
+import { StateProvider } from './UserStore'
 
 function App() {
   const [moreGenreListings, setMoreGenreListings] = React.useState({
@@ -18,25 +19,56 @@ function App() {
     setMoreGenreListings(listings)
   }
 
+  const initialUserState = {
+    id: '',
+    username: '',
+    wantsToDj: false,
+    chosenAvatarId: '',
+  }
+
+  const reducer = (state, action) => {
+    console.log(state, action)
+    switch (action.type) {
+      case 'updateUser':
+        return {
+          ...state,
+          wantsToDj: action.wantsToDj,
+          username: action.username,
+          chosenAvatarId: action.chosenAvatarId,
+        }
+
+      case 'logIn':
+        return {
+          ...state,
+          id: action.userId,
+        }
+
+      default:
+        return state
+    }
+  }
+
   return (
     <BrowserRouter>
-      <main>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/account" component={Account} />
-          <Route path="/room" component={Room} />
-          <Route exact path="/dashboard/addRoom">
-            <Dashboard />
-            <AddRoom />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard onShowMoreListings={onShowMoreListings} />
-          </Route>
-          <Route path="/dashboard/:genre">
-            <Genre {...moreGenreListings} />
-          </Route>
-        </Switch>
-      </main>
+      <StateProvider initialState={initialUserState} reducer={reducer}>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/account" component={Account} />
+            <Route path="/room" component={Room} />
+            <Route exact path="/dashboard/addRoom">
+              <Dashboard />
+              <AddRoom />
+            </Route>
+            <Route exact path="/dashboard">
+              <Dashboard onShowMoreListings={onShowMoreListings} />
+            </Route>
+            <Route path="/dashboard/:genre">
+              <Genre {...moreGenreListings} />
+            </Route>
+          </Switch>
+        </main>
+      </StateProvider>
     </BrowserRouter>
   )
 }
--- src/accountSetup/index.js
+++ src/accountSetup/index.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TextInput from '../components/TextInput'
 import Toggle from 'react-toggle'
 import 'react-toggle/style.css'
+import { Link } from 'react-router-dom'
+import { useUserState } from '../UserStore'
 
 const images = [
   require('../resources/img/avatar/WhiteMale.png'),
@@ -25,64 +27,68 @@ const animations = [
   require('../resources/img/dj-animations/DJ Black Female.gif'),
 ]
 
-class Account extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: '',
-      selected: 0,
-      dj: true,
-    }
+const Account = () => {
+  const [user, dispatch] = useUserState()
+  const [username, setUsername] = React.useState(user.username)
+  const [dj, setDj] = React.useState(user.wantsToDj)
+  const [chosenAvatarId, setChosenAvatarId] = React.useState(
+    user.chosenAvatarId
+  )
+
+  const onChange = e => {
+    setUsername(e.target.value)
   }
 
-  _onChange = e => {
-    this.setState({ username: e.target.value })
+  const handleToggle = e => {
+    setDj(e.target.checked)
   }
 
-  _handleToggle = e => {
-    this.setState({ dj: e.target.checked })
+  const onSubmit = () => {
+    dispatch({
+      type: 'updateUser',
+      username,
+      chosenAvatarId,
+      wantsToDj: dj,
+    })
   }
 
-  render() {
-    return (
-      <div id="account" className="center">
-        <TextInput
-          id="account_username"
-          label="Username"
-          onChange={this._onChange}
-          value={this.state.username}
-        />
-        <br />
-        <label>Select an avatar</label>
-        <div className="avatar-row center">
-          {images.map((item, index) => (
-            <img
-              onClick={() => this.setState({ selected: index })}
-              src={this.state.selected === index ? animations[index] : item}
-              style={{
-                height: this.state.selected === index ? 120 : 110,
-              }}
-              className="avatar"
-              key={index}
-              alt={index}
-            />
-          ))}
-        </div>
-        <br />
-        <label className="toggle-wrapper">
-          <span>Do you want to be a DJ?</span>
-          <Toggle
-            defaultChecked={this.state.dj}
-            onChange={this._handleToggle}
+  return (
+    <div id="account" className="center">
+      <TextInput
+        id="account_username"
+        label="Username"
+        onChange={onChange}
+        value={username}
+      />
+      <br />
+      <label>Select an avatar</label>
+      <div className="avatar-row center">
+        {images.map((item, index) => (
+          <img
+            onClick={() => setChosenAvatarId(index)}
+            src={chosenAvatarId === index ? animations[index] : item}
+            style={{
+              height: chosenAvatarId === index ? 120 : 110,
+            }}
+            className="avatar"
+            key={index}
+            alt={index}
           />
-        </label>
-        <br />
-        {this.state.username.length > 0 && (
-          <button className="btn-block">Start listening</button>
-        )}
+        ))}
       </div>
-    )
-  }
+      <br />
+      <label className="toggle-wrapper">
+        <span>Do you want to be a DJ?</span>
+        <Toggle defaultChecked={dj} onChange={handleToggle} />
+      </label>
+      <br />
+      {username.length > 0 && (
+        <Link to="/dashboard" className="btn-block" onClick={onSubmit}>
+          Start listening
+        </Link>
+      )}
+    </div>
+  )
 }
 
 export default Account
