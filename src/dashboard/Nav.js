3573e72d5c354010b8905e739f37235356adc1fc
Holben888 <holben888@gmail.com>
2019-11-17 03:37:07 -0500

    add genre search

--- src/dashboard/Nav.js
+++ src/dashboard/Nav.js
@@ -4,18 +4,25 @@ import { IoIosAdd as AddIcon } from 'react-icons/io'
 import { Link } from 'react-router-dom'
 import TextInput from '../components/TextInput'
 
-const Nav = () => (
-  <nav>
-    <Link to="/account">
-      <ProfileIcon size={40} />
-    </Link>
-    <div className="genre-search-container">
-      <TextInput id="search_genre" label="Search" />
-    </div>
-    <Link to="/dashboard/addRoom">
-      <AddIcon size={40} />
-    </Link>
-  </nav>
-)
+const Nav = ({ onSearch }) => {
+  const onChangeSearch = e => {
+    const newSearch = e.target.value
+    onSearch(newSearch)
+  }
+
+  return (
+    <nav>
+      <Link to="/account">
+        <ProfileIcon size={40} />
+      </Link>
+      <div className="genre-search-container">
+        <TextInput id="search_genre" label="Search" onChange={onChangeSearch} />
+      </div>
+      <Link to="/dashboard/addRoom">
+        <AddIcon size={40} />
+      </Link>
+    </nav>
+  )
+}
 
 export default Nav
