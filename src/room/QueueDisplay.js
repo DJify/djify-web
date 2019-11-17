557d16e2d6ee3dbe44e5ead1d717e4fb493eb5b0
Adib Khan <adib.khan@gmail.com>
2019-11-17 01:45:42 -0500

    Queue

--- src/room/QueueDisplay.js
+++ src/room/QueueDisplay.js
@@ -1,5 +1,45 @@
-import React from 'react';
-import { IoIosThumbsUp, IoIosThumbsDown } from 'react-icons/io';
+import React, { Component } from 'react';
+import { IoIosMove } from 'react-icons/io';
+import DraggableList from 'react-draggable-list';
 
-const JudgeDisplay = (props) => <div>
-</div>;
+class Outlet extends React.Component {
+
+  getDragHeight() {
+    return 40;
+  }
+
+  render() {
+    const {item, dragHandleProps} = this.props;
+    console.log(this.props);
+
+    return (
+      <div {...dragHandleProps} className="queue-cell">
+        <b>
+          { item.title }
+          <br />
+          { item.artist }
+        </b>
+        <IoIosMove size={30}/>
+      </div>
+    );
+  }
+}
+
+class QueueDisplay extends Component {
+  render() {
+    return(
+      <div>
+        <hr />
+        <div id="queue-container" ref={(i)=>this._list = i}>
+          <DraggableList list={this.props.songs}
+                         itemKey="title"
+                         template={Outlet}
+                         onMoveEnd={this.props.handleOrder}
+                         container={()=>this._list}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default QueueDisplay;
